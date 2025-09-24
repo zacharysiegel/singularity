1baed823754d4ec827f66c61324d148382816f00
zacharysiegel <siegelzc@gmail.com>
2025-09-23 21:57:32 -0500

    fix tabs

--- src/engine.cpp
+++ src/engine.cpp
@@ -14,11 +14,11 @@ static Font custom_font;
 static void draw();
 
 result_t init() {
-	SetTraceLogLevel(LOG_DEBUG);
+    SetTraceLogLevel(LOG_DEBUG);
     SetTargetFPS(app::TARGET_FPS);
 
     InitWindow(800, 600, application_name.c_str());
-	// todo: SetWindowIcon
+    // todo: SetWindowIcon
 
     default_font = GetFontDefault();
     custom_font = LoadFont("/Users/zacharysiegel/Downloads/Google_Sans_Code/static/GoogleSansCode-Regular.ttf");
@@ -37,9 +37,9 @@ result_t destroy() {
 
 result_t run() {
     while (!WindowShouldClose()) {
-		if (IsKeyPressed(KEY_A)) {
-			TraceLog(LOG_DEBUG, "a pressed");
-		}
+        if (IsKeyPressed(KEY_A)) {
+            TraceLog(LOG_DEBUG, "a pressed");
+        }
 
         BeginDrawing();
         draw();
@@ -51,16 +51,16 @@ result_t run() {
 static void draw() {
     ClearBackground(Color{.r = 30, .g = 30, .b = 30, .a = 0xFF});
 
-	// Text drawing is not working (even copying the Raylib default font example fails)
+    // Text drawing is not working (even copying the Raylib default font example fails)
     DrawText("test text 1", 10, 10, 20, Color{0xff, 0xff, 0x80, 0xff});
     DrawTextEx(app::default_font, "test text 2", Vector2{.x = 10, .y = 10}, 20.0f, 10.0f, Color{0xff, 0xff, 0x80, 0xff});
     DrawTextEx(app::custom_font, "test text 3", Vector2{.x = 10, .y = 10}, 20.0f, 10.0f, Color{0xff, 0xff, 0x80, 0xff});
-	DrawTextPro(app::default_font, "test text 5", Vector2{10, 10}, Vector2{50, 50}, 0.0f, 20.0f, 10.0f, Color{0xff, 0xff, 0x80, 0xff});
-	DrawTextPro(app::custom_font, "test text 5", Vector2{10, 10}, Vector2{50, 50}, 0.0f, 20.0f, 10.0f, Color{0xff, 0xff, 0x80, 0xff});
+    DrawTextPro(app::default_font, "test text 5", Vector2{10, 10}, Vector2{50, 50}, 0.0f, 20.0f, 10.0f, Color{0xff, 0xff, 0x80, 0xff});
+    DrawTextPro(app::custom_font, "test text 5", Vector2{10, 10}, Vector2{50, 50}, 0.0f, 20.0f, 10.0f, Color{0xff, 0xff, 0x80, 0xff});
 
-	// todo: draw background
-	
-	// todo: draw map
+    // todo: draw background
+
+    // todo: draw map
 }
 
 } // namespace app
