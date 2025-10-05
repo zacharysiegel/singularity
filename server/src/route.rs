048964f1866b8aa874a32c10eaee43cb89e672af
zacharysiegel <siegelzc@gmail.com>
2025-10-04 20:04:48 -0500

    pedantry

--- server/src/route.rs
+++ server/src/route.rs
@@ -7,46 +7,48 @@ pub async fn route_frame(connection: &Connection, frame: Frame) {
     match frame.head.op_type {
         OperationType::Heartbeat => {
             log::trace!("Heartbeat received; [{}] [{}]", connection, frame);
-            heartbeat(connection, frame);
+            heartbeat(frame);
         }
         OperationType::Register => {
             log::trace!("Register received; [{}] [{}]", connection, frame);
-            register(connection, frame);
+            register(frame);
         }
         OperationType::Acknowledgement => {
             log::trace!("Acknowledgement received; [{}] [{}]", connection, frame);
-            acknowledgement(connection, frame);
+            acknowledgement(frame);
         }
         OperationType::_PlaceholderDynamic => {
             log::trace!("_PlaceholderDynamic received; [{}] [{}]", connection, frame);
-            _placeholder_dynamic(connection, frame);
+            _placeholder_dynamic(frame);
         }
     }
 }
 
-#[allow(unused_variables)]
-fn heartbeat(connection: &Connection, frame: Frame) {
+fn heartbeat(frame: Frame) {
     let heartbeat: Heartbeat = Heartbeat::from(&frame);
     log::debug!("parsed frame; [{:?}]", heartbeat);
 }
 
-#[allow(unused_variables)]
-fn register(connection: &Connection, frame: Frame) {
+fn register(frame: Frame) {
     let register: Register = Register::from(&frame);
     log::debug!("parsed frame; [{:?}]", register);
+
+
     todo!();
 }
 
-#[allow(unused_variables)]
-fn acknowledgement(connection: &Connection, frame: Frame) {
+fn acknowledgement(frame: Frame) {
     let acknowledgement: Acknowledgement = Acknowledgement::from(&frame);
     log::debug!("parsed frame; [{:?}]", acknowledgement);
+
+
     todo!();
 }
 
-#[allow(unused_variables)]
-fn _placeholder_dynamic(connection: &Connection, frame: Frame) {
+fn _placeholder_dynamic(frame: Frame) {
     let _placeholder_dynamic: _PlaceholderDynamic = _PlaceholderDynamic::from(&frame);
     log::debug!("parsed frame; [{:?}]", _placeholder_dynamic);
+
+
     todo!();
 }
