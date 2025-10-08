981399e9b4c653d876e4e5f19d5a33d2ccc33dbf
zacharysiegel <siegelzc@gmail.com>
2025-10-08 00:22:12 -0500

    todo

--- shared/src/network/monitor.rs
+++ shared/src/network/monitor.rs
@@ -4,6 +4,8 @@ use tokio::sync::{RwLockWriteGuard};
 use crate::network::connection::{Connection, ConnectionReader};
 use crate::network::protocol::Frame;
 
+// todo: if this function is the only user of ConnectionReader, it can be given full ownership and obviate the lock acquisition
+//  It's worth trying to find a way to have single ownership of the ConnectionWriter as well
 pub async fn monitor_incoming_frames<F, Fut>(connection: Arc<Connection>, callback: F)
 where
     F: Fn(Arc<Connection>, Frame) -> Fut,
