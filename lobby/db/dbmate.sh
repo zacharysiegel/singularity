c7b08dacc43f5f3820d31ab929d3e765db8205c5
zacharysiegel <siegelzc@gmail.com>
2025-11-11 14:34:43 -0600

    pedantry

--- lobby/db/dbmate.sh
+++ lobby/db/dbmate.sh
@@ -8,4 +8,4 @@ cd "${repo_dir}"
 source ./.env
 
 DBMATE_GLOBAL_OPTIONS=(--migrations-dir './lobby/db/migrations' --wait --url "$DATABASE_URL/lobby?sslmode=disable")
-dbmate "${DBMATE_GLOBAL_OPTIONS[@]}" "${@?Must provide dbmate command (and optionally command options)}"
+dbmate "${DBMATE_GLOBAL_OPTIONS[@]}" "${@}"
