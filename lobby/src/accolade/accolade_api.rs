3a8f24317bb33cbd06ffddcbca0157433579f9e1
zacharysiegel <siegelzc@gmail.com>
2026-04-16 23:00:30 -0500

    formatting

--- lobby/src/accolade/accolade_api.rs
+++ lobby/src/accolade/accolade_api.rs
@@ -1,21 +1,15 @@
-use actix_web::{web, HttpRequest, HttpResponse};
-use shared::schema::accolade::AccoladeSerial;
-use sqlx::PgPool;
-
-use crate::http;
 use super::accolade_db;
 use super::accolade_model::{Accolade, AccoladeEntity};
+use crate::http;
+use actix_web::{HttpRequest, HttpResponse, web};
+use shared::schema::accolade::AccoladeSerial;
+use sqlx::PgPool;
+use uuid::Uuid;
 
 pub fn configurer(config: &mut web::ServiceConfig) {
     config
-        .service(
-            web::resource("/account/{account_id}/accolades")
-                .route(web::get().to(get_account_accolades)),
-        )
-        .service(
-            web::resource("/game/{game_id}/accolades")
-                .route(web::get().to(get_game_accolades)),
-        );
+        .service(web::resource("/account/{account_id}/accolades").route(web::get().to(get_account_accolades)))
+        .service(web::resource("/game/{game_id}/accolades").route(web::get().to(get_game_accolades)));
 }
 
 async fn get_account_accolades(
@@ -24,30 +18,32 @@ async fn get_account_accolades(
     path: web::Path<(String,)>,
 ) -> HttpResponse {
     let (account_id_string,): (String,) = path.into_inner();
-    let account_id: uuid::Uuid = unwrap_or_400!(uuid::Uuid::parse_str(&account_id_string));
+    let account_id: Uuid = unwrap_or_400!(Uuid::parse_str(&account_id_string));
 
-    let accolade_entities: Vec<AccoladeEntity> = unwrap_or_500!(accolade_db::get_accolades_by_account(pool.get_ref(), account_id).await);
-    let accolade_serials: Vec<AccoladeSerial> = unwrap_or_500!(accolade_entities
-        .into_iter()
-        .map(|entity| Accolade::try_from(entity).map(|accolade| AccoladeSerial::from(&accolade)))
-        .collect::<Result<Vec<_>, _>>());
+    let accolade_entities: Vec<AccoladeEntity> =
+        unwrap_or_500!(accolade_db::get_accolades_by_account(pool.get_ref(), account_id).await);
+    let accolade_serials: Vec<AccoladeSerial> = unwrap_or_500!(
+        accolade_entities
+            .into_iter()
+            .map(|entity| Accolade::try_from(entity).map(|accolade| AccoladeSerial::from(&accolade)))
+            .collect::<Result<Vec<_>, _>>()
+    );
 
     http::serialize_response(&request, &accolade_serials)
 }
 
-async fn get_game_accolades(
-    request: HttpRequest,
-    pool: web::Data<PgPool>,
-    path: web::Path<(String,)>,
-) -> HttpResponse {
+async fn get_game_accolades(request: HttpRequest, pool: web::Data<PgPool>, path: web::Path<(String,)>) -> HttpResponse {
     let (game_id_string,): (String,) = path.into_inner();
-    let game_id: uuid::Uuid = unwrap_or_400!(uuid::Uuid::parse_str(&game_id_string));
+    let game_id: Uuid = unwrap_or_400!(Uuid::parse_str(&game_id_string));
 
-    let accolade_entities: Vec<AccoladeEntity> = unwrap_or_500!(accolade_db::get_accolades_by_game(pool.get_ref(), game_id).await);
-    let accolade_serials: Vec<AccoladeSerial> = unwrap_or_500!(accolade_entities
-        .into_iter()
-        .map(|entity| Accolade::try_from(entity).map(|accolade| AccoladeSerial::from(&accolade)))
-        .collect::<Result<Vec<_>, _>>());
+    let accolade_entities: Vec<AccoladeEntity> =
+        unwrap_or_500!(accolade_db::get_accolades_by_game(pool.get_ref(), game_id).await);
+    let accolade_serials: Vec<AccoladeSerial> = unwrap_or_500!(
+        accolade_entities
+            .into_iter()
+            .map(|entity| Accolade::try_from(entity).map(|accolade| AccoladeSerial::from(&accolade)))
+            .collect::<Result<Vec<_>, _>>()
+    );
 
     http::serialize_response(&request, &accolade_serials)
 }
