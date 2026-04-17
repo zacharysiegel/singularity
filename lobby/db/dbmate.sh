6345a30af0986e261e0676b38a73bf6c307f7d71
zacharysiegel <siegelzc@gmail.com>
2026-04-16 23:00:21 -0500

    sqlx prepare in dbmate script

--- lobby/db/dbmate.sh
+++ lobby/db/dbmate.sh
@@ -7,5 +7,10 @@ cd "${repo_dir}"
 
 source ./.env
 
+echo "Executing dbmate"
 DBMATE_GLOBAL_OPTIONS=(--migrations-dir './lobby/db/migrations' --wait --url "$DATABASE_URL?sslmode=disable")
 dbmate "${DBMATE_GLOBAL_OPTIONS[@]}" "${@}"
+
+echo "Regenerating sqlx caches"
+cargo sqlx prepare --workspace
+
