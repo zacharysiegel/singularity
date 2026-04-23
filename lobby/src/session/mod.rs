c76c7e7a57c01ce750ea0fcf5e587d108725c4cf
zacharysiegel <siegelzc@gmail.com>
2026-04-22 19:11:52 -0500

    pedantry

--- lobby/src/session/mod.rs
+++ lobby/src/session/mod.rs
@@ -1,8 +1,10 @@
+use chrono::Duration;
+
 pub(crate) mod session_db;
 
 pub mod session_api;
 pub mod session_extractor;
 pub mod session_model;
 
-pub const SESSION_DURATION: chrono::Duration = chrono::Duration::days(14);
-pub const SESSION_REFRESH_THRESHOLD: chrono::Duration = chrono::Duration::days(13);
+pub const SESSION_DURATION: Duration = Duration::days(14);
+pub const SESSION_REFRESH_THRESHOLD: Duration = Duration::days(13);
