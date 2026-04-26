f6c19c86d865133961b6edb180d17ad9078b8beb
zacharysiegel <siegelzc@gmail.com>
2026-04-26 17:07:49 -0500

    pedantry

--- client/src/ws/route.rs
+++ client/src/ws/route.rs
@@ -1,3 +1,4 @@
+use std::sync::RwLockWriteGuard;
 use shared::schema::ws_message::{ConnectionType, WsEvent};
 use crate::conversation::event;
 use crate::state::STATE;
@@ -15,7 +16,7 @@ pub fn route_ws_event(connection_type: ConnectionType, ws_event: WsEvent) {
         }
         WsEvent::Error { message } => {
             log::warn!("WsEvent::Error [{connection_type}]: {message}");
-            let mut last_error = STATE.ws.last_error.write().unwrap();
+            let mut last_error: RwLockWriteGuard<Option<String>> = STATE.ws.last_error.write().unwrap();
             *last_error = Some(message);
         }
     }
