bbe4b41bf46027ad13c81b50aa1846a1c8587f00
zacharysiegel <siegelzc@gmail.com>
2026-04-26 22:05:48 -0500

    use millisecond precision logs

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,7 +6,11 @@ use std::error::Error;
 #[tokio::main]
 pub async fn main() -> Result<(), Box<dyn Error>> {
     environment::load_env()?;
-    env_logger::builder().filter_level(log::LevelFilter::Debug).format_source_path(true).try_init()?;
+    env_logger::builder()
+        .filter_level(log::LevelFilter::Debug)
+        .format_source_path(true)
+        .format_timestamp_millis()
+        .try_init()?;
 
     let (mut rl, rl_thread): (RaylibHandle, RaylibThread) = engine::init()?;
     engine::run(&mut rl, &rl_thread)?;
