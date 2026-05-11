9a84945420b895182083f77f43fc36fbffaca30b
zacharysiegel <siegelzc@gmail.com>
2026-05-10 23:14:59 -0500

    add center_vertically utility, use in header and entry text positioning

--- shared/src/math/rectangle.rs
+++ shared/src/math/rectangle.rs
@@ -13,3 +13,7 @@ pub fn rect_dimensions(rectangle: Rectangle) -> Vector2 {
         y: rectangle.height,
     }
 }
+
+pub fn center_vertically(container_y: f32, container_height: f32, content_height: f32) -> f32 {
+    container_y + (container_height - content_height) / 2.
+}
