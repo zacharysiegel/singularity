e4364fd66887c0b8e4524e7487cca7f64c14628b
zacharysiegel <siegelzc@gmail.com>
2026-05-12 22:50:27 -0500

    use VerticalScrollRegion::draw() instead of manual scissor mode

--- client/src/component/scroll_region.rs
+++ client/src/component/scroll_region.rs
@@ -72,7 +72,7 @@ impl VerticalScrollRegion {
         rl_draw.draw_scissor_mode(
             self.viewport.x as i32,
             self.viewport.y as i32,
-            self.viewport.width as i32,
+            self.viewport.width as i32 + 1, // prevent scissor from clipping the right border pixel
             self.viewport.height as i32,
             |scissor_draw: RaylibScissorMode<RaylibDrawHandle>| {
                 draw_fn(scissor_draw, y_offset);
